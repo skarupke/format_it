dec78973920dc4e07288abf350a839e005f2e07b
Malte Skarupke <malteskarupke@web.de>
2014-09-01 12:56:35 -0400

    Added license.

--- format_float.cpp
+++ format_float.cpp
@@ -1,3 +1,26 @@
+/*
+This is free and unencumbered software released into the public domain.
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+For more information, please refer to <http://unlicense.org/>
+*/
+
 #include "format_float.hpp"
 
 #ifndef DISABLE_GTEST
@@ -210,7 +233,7 @@ struct UnsignedAsFloat
 };
 
 constexpr unsigned num_iterations = 200000;
-TEST(format_float, printf_performance)
+TEST(format_float, DISABLED_printf_performance)
 {
 	for (unsigned i = 0; i < num_iterations; ++i)
 	{
@@ -218,7 +241,7 @@ TEST(format_float, printf_performance)
 		snprintf(buffer, sizeof(buffer) / sizeof(*buffer), "%g, %g, %g, %g", static_cast<float>(i), std::pow(i, 1.45123), i * 0.5, UnsignedAsFloat(i).as_float);
 	}
 }
-TEST(format_float, format_float_performance)
+TEST(format_float, DISABLED_format_float_performance)
 {
 	for (unsigned i = 0; i < num_iterations; ++i)
 	{
--- stack_format.cpp
+++ stack_format.cpp
@@ -1,3 +1,26 @@
+/*
+This is free and unencumbered software released into the public domain.
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+For more information, please refer to <http://unlicense.org/>
+*/
+
 #include "stack_format.hpp"
 
 #ifndef DISABLE_GTEST
@@ -114,32 +137,32 @@ TEST(stack_format, overflow_callback_zero_sized)
 	ASSERT_FALSE(did_trigger);
 }
 static constexpr int profile_count = 2000000;
-TEST(stack_format, profile)
+TEST(stack_format, DISABLED_profile)
 {
 	for (int i = 0; i < profile_count; ++i)
 	{
 		//fmt::stack_format<1024>("[%0, %1, %2]", i, i * 2, i / 2);
-		//fmt::stack_format<1024>("[%0, %1, %2]", static_cast<float>(i), i * 2.0f, i / 2.0f);
-		fmt::stack_format<1024>("[%0, %1, %2]", "foo", "bar", "baz");
+		fmt::stack_format<1024>("[%0, %1, %2]", static_cast<float>(i), i * 2.0f, i / 2.0f);
+		//fmt::stack_format<1024>("[%0, %1, %2]", "foo", "bar", "baz");
 	}
 }
-TEST(stack_format, profile_printf)
+TEST(stack_format, DISABLED_profile_printf)
 {
 	for (int i = 0; i < profile_count; ++i)
 	{
 		char buffer[1024];
 		//snprintf(buffer, 1024, "[%d, %d, %d]", i, i * 2, i / 2);
-		//snprintf(buffer, 1024, "[%f, %f, %f]", static_cast<float>(i), i * 2.0f, i / 2.0f);
-		snprintf(buffer, 1024, "[%s, %s, %s]", "foo", "bar", "baz");
+		snprintf(buffer, 1024, "[%g, %g, %g]", static_cast<float>(i), i * 2.0f, i / 2.0f);
+		//snprintf(buffer, 1024, "[%s, %s, %s]", "foo", "bar", "baz");
 	}
 }
-TEST(stack_format, profile_stringstream)
+TEST(stack_format, DISABLED_profile_stringstream)
 {
 	for (int i = 0; i < profile_count; ++i)
 	{
 		//std::stringstream() << '[' << i << ", " << i * 2 << ", " << i / 2 << ']';
-		//std::stringstream() << '[' << static_cast<float>(i) << ", " << i * 2.0f << ", " << i / 2.0f << ']';
-		std::stringstream() << '[' << "foo" << ", " << "bar" << ", " << "baz" << ']';
+		std::stringstream() << '[' << static_cast<float>(i) << ", " << i * 2.0f << ", " << i / 2.0f << ']';
+		//std::stringstream() << '[' << "foo" << ", " << "bar" << ", " << "baz" << ']';
 	}
 }
 }
